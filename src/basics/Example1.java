f9e2f96ade22e81e1c891700ebd0f1ef03e85a83
veelinktech <veelinktechnologies@gmail.com>
2023-11-09 15:18:34 +0530

    Second commit

--- src/basics/Example1.java
+++ src/basics/Example1.java
@@ -1,9 +1,22 @@
 package basics;
 
-public class Example1 {
+public class Example1 
+{
+	
+	public void add()
+	{
+		int a=10,b=20,c;
+		
+		c=a+b;
+		
+		System.out.println("Add result = "+  c);
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Example1 e1=new Example1();
+		
+		e1.add();
 
 	}
 
