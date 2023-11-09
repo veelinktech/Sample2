6350b99c275d820c22dd414c42262d0a5a62585b
veelinktech <104140334+veelinktech@users.noreply.github.com>
2023-11-09 15:20:36 +0530

    Create Example2.java

---
+++ src/basics/Example2.java
@@ -0,0 +1,23 @@
+package basics;
+
+public class Example1 
+{
+	
+	public void sub()
+	{
+		int a=10,b=20,c;
+		
+		c=a-b;
+		
+		System.out.println("sub result = "+  c);
+	}
+
+	public static void main(String[] args) 
+	{
+		Example1 e1=new Example1();
+		
+		e1.sub();
+
+	}
+
+}
